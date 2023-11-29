85a408d5161320e8000a3311b0538e050a3810ff
Tauraim <tmuzembe@outlook.com>
2023-11-28 17:24:52 -0800

    Loops

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -6,10 +6,4 @@ if number > 0:
 elif number == 0:
     print("{} is zero".format(number))
 elif number < 0:
-    print("{} is negative".format(number))
-
-
-
-
-
-vi 1-last_digit.py
+    print("{} is negative".format(number)
