cd718e0dbb1d3a1b026712ad2c4595abf918faea
Tauraim <tmuzembe@outlook.com>
2024-01-10 11:53:30 -0800

    test

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-"""Defines an inherited class-checking function"""
+"""Defines an inherited class-checking function."""
+
 
 def inherits_from(obj, a_class):
-    """checks if an object is an inherited instance of a class
+    """Checks if an object is an inherited instance of a class.
 
     Args:
         obj (any): The object to check.
-        a_class (type) :the class to match of obj to.
+        a_class (type): The class to match the type of obj to.
     Returns:
-        if obj is inherited instance of a_class - True.
-        otherwise - False.
+        If obj is an inherited instance of a_class - True.
+        Otherwise - False.
     """
-
-    if issubclass(type(obj), a_class) and type(obj) !+ a_class:
-        return True 
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
     return False
